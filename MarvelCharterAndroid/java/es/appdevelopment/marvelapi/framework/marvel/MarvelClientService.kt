5bddfb1badf40ac7914b5593d80f6167b4f3c98d
neoxp <emili.m.fores@gmail.com>
2020-11-13 11:34:04 +0100

    Add files via upload

---
+++ MarvelCharterAndroid/java/es/appdevelopment/marvelapi/framework/marvel/MarvelClientService.kt
@@ -0,0 +1,76 @@
+package es.appdevelopment.marvelapi.framework.marvel
+
+import es.appdevelopment.domain.Superheroe
+import es.appdevelopment.marvelapi.framework.marvel.model.Character
+import es.appdevelopment.marvelapi.framework.marvel.model.toSuperheroe
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class MarvelClientService {
+    private val URL_BASE = "https://gateway.marvel.com:443"
+    private val PUBLIC_KEY = "12ff4a1c748b90b56d04980bd6309239"
+    private val SECRET_KEY = "bac19de47a71b63240d8c83eb172134eb8cd58fb"
+
+    private val marvelClient: MarvelClient
+    private var timeStamp: String = Date().time.toString()
+
+    init {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .addInterceptor(interceptor) // TODO Borrar producción
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(URL_BASE)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        marvelClient = retrofit.create(MarvelClient::class.java)
+    }
+
+    suspend fun getCharacters(offset: Int, limit: Int, orderBy: String): List<Superheroe> {
+        val msa = getDataForMarvelServerAuthentication()
+
+        try {
+            val response = marvelClient.getCharacters(PUBLIC_KEY, limit, offset, timeStamp, msa, orderBy)
+
+            val results: MutableList<Character> = response.data.results
+            return results.map { it.toSuperheroe() }
+        } catch (exception: Exception) {
+
+        }
+
+        return emptyList()
+    }
+
+    suspend fun getCharacter(superheroeId: Int): Superheroe? {
+        val msa = getDataForMarvelServerAuthentication()
+
+        try {
+            val response = marvelClient.getCharacter("$superheroeId", PUBLIC_KEY, timeStamp, msa)
+
+            val results: MutableList<Character> = response.data.results
+            return results.map { it.toSuperheroe() }.get(0)
+        } catch (exception: Exception) {
+
+        }
+
+        return null
+    }
+
+    private fun getDataForMarvelServerAuthentication(): String {
+        timeStamp = Date().time.toString()
+
+        return (timeStamp + SECRET_KEY + PUBLIC_KEY).md5()
+    }
+
+
+}
---
+++ MarvelCharterAndroid/java/es/appdevelopment/marvelapi/ui/presenters/MainPresenter.kt
@@ -0,0 +1,122 @@
+package es.appdevelopment.marvelapi.ui.presenters
+
+import es.appdevelopment.domain.Ordenation
+import es.appdevelopment.marvelapi.R
+import es.appdevelopment.marvelapi.ui.model.MarvelSuperheroeForList
+import es.appdevelopment.marvelapi.ui.model.toMarvelSuperheroeFromList
+import es.appdevelopment.usecases.GetOrdenation
+import es.appdevelopment.usecases.ListSuperheroes
+import es.appdevelopment.usecases.SaveOrdenation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainPresenter(
+    private var view: ViewMain?,
+    private val listSuperheroes: ListSuperheroes,
+    private val getOrdenation: GetOrdenation,
+    private val saveOrdenation: SaveOrdenation
+) {
+    private val limit: Int = 50
+    private var offset: Int = 0
+    private lateinit var orderBy: Ordenation
+
+    fun create() {
+        view?.showLoading()
+
+        getSavedOrdenation()
+
+        renderSuperheroes()
+    }
+
+    private fun renderSuperheroes() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val superheroes = withContext(Dispatchers.IO) { listSuperheroes(offset, limit, orderBy.typeOrdenation) }
+            view?.hideLoading()
+
+            when {
+                superheroes.isEmpty() -> {
+                    if(offset == 0) {
+                        view?.showEmptyCase()
+                    }
+                }
+                else -> {
+                    view?.hideEmptyCase()
+
+                    val listMarvelSuperheroe = superheroes.map { it.toMarvelSuperheroeFromList() }
+                    view?.listSuperheroes(listMarvelSuperheroe)
+                }
+            }
+
+        }
+    }
+
+    private fun getSavedOrdenation() {
+        orderBy = getOrdenation()
+    }
+
+    fun getSuperheroesWithOrdenation() {
+        view?.showLoading()
+        view?.cleanSuperheroes()
+
+        offset = 0
+
+        when (orderBy) {
+            Ordenation.NAME -> {
+                orderBy = Ordenation.MODIFIED
+            }
+
+            Ordenation.MODIFIED -> {
+                orderBy = Ordenation.NAME
+            }
+        }
+
+        calculateIconMenu()
+        renderSuperheroes()
+        saveNewOrdenation(orderBy)
+    }
+
+    fun getMoreSuperheroes() {
+        offset = offset.plus(limit)
+        renderSuperheroes()
+    }
+
+    fun calculateIconMenu() {
+        when (orderBy) {
+            Ordenation.NAME -> {
+                view?.changeIconMenu(R.mipmap.orderby_date)
+            }
+
+            Ordenation.MODIFIED -> {
+                view?.changeIconMenu(R.mipmap.orderby_alphabet)
+            }
+        }
+    }
+
+    private fun saveNewOrdenation(ordenation: Ordenation) {
+        saveOrdenation(ordenation)
+    }
+
+    fun onSuperHeroClicked(marvelSuperHeroeId: Int) = view?.showSuperheroe(marvelSuperHeroeId)
+
+    fun onDestroy() {
+        view = null
+    }
+
+    interface ViewMain {
+        fun hideLoading()
+        fun showLoading()
+
+        fun listSuperheroes(marvelSuperHeroes:  List<MarvelSuperheroeForList>)
+        fun showSuperheroe(marvelSuperHeroeId: Int)
+        fun cleanSuperheroes()
+
+        fun changeIconMenu(iconId: Int)
+
+        fun showToastMessage(stringId: Int)
+
+        fun showEmptyCase()
+        fun hideEmptyCase()
+    }
+}
